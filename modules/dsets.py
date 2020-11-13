15f6178c6467c45972b9085bda2a77b79c9441d9
peterbacalso <peterkarlca@gmail.com>
2020-11-12 20:13:28 -0500

    created lesions locations metadata

--- modules/dsets.py
+++ modules/dsets.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import feather
+import functools
 import numpy as np
 import pandas as pd
 import nibabel as nib
@@ -33,27 +34,38 @@ class Ct:
 
             seg = nib.load(ct_paths[1])
             self.mask = seg.get_fdata()
+        self.lesions = get_lesions_dict()[uid]
 
 
-    def group_lesions(self):
+    def group_lesions(self, output_df=True):
         # group blobs using morphology erosion
-        candidate_label_a, candidate_count = ndimage.label(self.mask) 
+        lesion_label_a, lesion_count = ndimage.label(self.mask) 
         center_irc_list = ndimage.center_of_mass(
-            self.hu.clip(-1000,1000) + 1001, # set positive to match the functions expectation of non-negative mass
-            labels = candidate_label_a,
-            index=np.arange(1,candidate_count+1))
+            self.hu.clip(-1000,1000) + 1001, # function needs +'ve input
+            labels = lesion_label_a,
+            index=np.arange(1,lesion_count+1))
+        lesion_cols = ['uid', 'coordI', 'coordR', 'coordC']
+        lesions = []
         # center_of_mass will produce floating point values so we round
-        candidate_cols = ['uid', 'coordI', 'coordR', 'coordC']
-        candidates = []
         for i, center_irc in enumerate(center_irc_list):
-            candidates.append([self.uid, 
+            lesions.append([self.uid, 
                                int(center_irc[0]), 
                                int(center_irc[1]), 
                                int(center_irc[2])])
 
-        return pd.DataFrame(candidates, columns=candidate_cols)
+        return lesions if not output_df \
+            else pd.DataFrame(lesions, columns=lesion_cols)
 
 
+@functools.lru_cache(1)
+def get_lesions_dict():
+    df_lesions = pd.read_feather('../df_lesion_coords.fth')
+    lesions_dict = {}
+
+    for _, lesion in df_lesions.iterrows():
+        lesions_dict.setdefault(lesion.uid, []).append(lesion)
+
+    return lesions_dict 
 
 
 
